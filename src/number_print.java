305aade07d872e660381d779f47913fba16ac6f4
Ashu.Vishwakarma <aashuvis123@gmail.com>
2024-03-11 17:11:40 +0530

    first commit

---
+++ src/number_print.java
@@ -0,0 +1,9 @@
+public class number_print {
+    public  static void main(String[] args){
+        int i =0;
+        int j = 100;
+        for (i = 0; i <= j; i++){
+            System.out.println(i);
+        }
+    }
+}
